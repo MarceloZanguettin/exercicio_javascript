01e629e1740ddebfdc9e22822833c277be4451d1
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-10-12 19:56:22 -0300

    aprendendo a usar JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/*
 function livro(nome, ano, autor) {
-  const nomeMaiusculo = nome.toUpperCase;
+  const nomeMaiusculo = nome.toUpperCase();
+  const totalAnos = 2050 - ano;
+  const frase = nome + " por " + autor;
+  const objeto = {
+    nome: nomeMaiusculo,
+    totalAnos,
+    frase,
+  };
+  return objeto;
 }
+*/
+
+function livro(nome, ano, autor) {
+  return {
+    nome: nome.toUpperCase(),
+    totalAnos: 2050 - ano,
+    frase: nome + " por " + autor,
+  };
+}
+
+const livroRetorno = livro("O Senhor dos Aneis", 1954, "J. R. R. Tolkien");
+
+console.log(livroRetorno.frase);
